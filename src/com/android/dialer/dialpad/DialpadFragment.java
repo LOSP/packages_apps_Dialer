da53b24b924adbe10b27fc62d696a8ef5845766c
martincz <martincz@mokeetek.com>
2013-08-22 02:10:44 +0800

    Dialer: backup
    Change-Id: I62f61312a510d6d7f3d0a4a2df98d02494770f5c

--- src/com/android/dialer/dialpad/DialpadFragment.java
+++ src/com/android/dialer/dialpad/DialpadFragment.java
@@ -1771,7 +1771,7 @@ public class DialpadFragment extends Fragment
         return mDigits.getText().toString().replace("-", "").replace("(", "").replace(")", "").replace("+", "").replace("*", "").replace("#", "").length();
     }
 
-    public static String getDigitsTest() {
+    public static String getDigitsText() {
         return mDigits.getText().toString().replace("-", "").replace("(", "").replace(")", "").replace("+", "").replace("*", "").replace("#", "");
     }
 }
